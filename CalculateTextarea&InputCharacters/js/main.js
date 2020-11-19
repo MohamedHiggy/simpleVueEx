46439e0a56cce27e801cc4a8b17e0a731f92d348
MohamedHiggy <mohamed.higgy31@gmail.com>
2020-11-19 23:31:40 +0200

    first commit

---
+++ CalculateTextarea&InputCharacters/js/main.js
@@ -0,0 +1,16 @@
+var app = new Vue({
+    el: "#Mou-app",
+    data: {
+        maxLenght: 20,
+        remainingChars: 20, 
+        message: null,
+        reachZero: false
+    },
+    methods: {
+        countChars: function () {
+            this.remainingChars = this.maxLenght - this.message.length;
+            this.reachZero = this.remainingChars === 0;
+            console.log(this.reachZero);
+        }
+    }
+});
