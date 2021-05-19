ce4d5bdd3c06460d45a2cea890c60559b995f0f4
hagooog <mohamed.higgy31@gmail.com>
2021-05-19 21:21:13 +0200

    handel form with components

--- CalculateTextarea&InputCharacters/js/main.js
+++ CalculateTextarea&InputCharacters/js/main.js
@@ -2,12 +2,12 @@ var app = new Vue({
     el: "#Mou-app",
     data: {
         maxLenght: 20,
-        remainingChars: 20, 
+        remainingChars: 20,
         message: null,
         reachZero: false
     },
     methods: {
-        countChars: function () {
+        countChars: function() {
             this.remainingChars = this.maxLenght - this.message.length;
             this.reachZero = this.remainingChars === 0;
             console.log(this.reachZero);
